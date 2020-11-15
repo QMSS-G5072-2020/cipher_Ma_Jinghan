9545c1b0fae8d176b788033abb311303430ca9f6
JinghanMa98 <jm5223@columbia.edu>
2020-11-15 19:16:55 +0800

    hw

--- tests/test_cipher_jm5223.py
+++ tests/test_cipher_jm5223.py
@@ -3,3 +3,10 @@ from cipher_jm5223 import cipher_jm5223
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_cipher():
+    example='happy'
+    shift=1
+    expected='ibqqz'
+    actual=cipher_jm5223.cipher(example,shift,encrypt=True)
+    assert actual==expected
